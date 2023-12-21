c46cb9977936af7d7445d53771f5a4e9b4084dad
Ezio121 <65387250+Ezio121@users.noreply.github.com>
2023-12-21 21:19:12 +0530

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,3 +1,5 @@
+-- fxmanifest.lua
+
 fx_version 'cerulean'
 game 'gta5'
 
@@ -5,7 +7,10 @@ author 'ezio786'
 description 'Car Engine Stalling and Dizzy Effect Mod for FiveM'
 version '1.0.0'
 
+dependencies 'ps-dispatch'
 client_script 'config.lua'
 client_script 'engine_stalling.lua'
+
+
 
 -- For Any issues Contact @ Discord (badassfalcon)
